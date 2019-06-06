d0562aa3a382c70d297442ac93391b8baa6e8af9
Vladimir Nikitin <unkaid@gmail.com>
2019-06-06 10:51:56 +0300

    Update rStreamColshapes.js

--- server-files/packages/yourGameMode/rStreamColshapes.js
+++ server-files/packages/yourGameMode/rStreamColshapes.js
@@ -1,18 +1,22 @@
 let rStreamColshape = mp.colshapes.newSphere(-410, 1179, 325, 10, 0);
 
+//fix by Loxitation, thanks to him
+
 function playerEnterColshapeHandler(player, shape) {
-	if(shape == rStreamColshape) {
-		player.notify(`Radio stream started`);
-	}
+    if(shape == rStreamColshape) {
+        player.notify(`Radio stream started`);
+        player.call("rStreamStart", []);
+    }
 }
 mp.events.add("playerEnterColshape", playerEnterColshapeHandler);
 
 function playerExitColshapeHandler(player, shape) {
-	if(shape == rStreamColshape) {
-		player.notify(`Radio stream stoped`);
-	}
+    if(shape == rStreamColshape) {
+        player.notify(`Radio stream stopped`);
+        player.call("rStreamStop", []);
+    }
 }
-mp.events.add("playerExitColshape", playerExitColshapeHandler);
+mp.events.add("playerExitColshape", playerExitColshapeHandler); 
 
 
 //optional
